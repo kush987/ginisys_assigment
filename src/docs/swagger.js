d6a15e19b720f99b21f851ad9b8af8e63b05fe06
kush987 <kushalrajak1999@gmail.com>
2025-05-12 00:21:58 +0530

    update the description of swagger

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -8,7 +8,7 @@ const options = {
     info: {
       title: 'Pet API',
       version: '1.0.0',
-      description: 'API for Pet'
+      description: 'Pet Api use -> access token for authoize pet apis'
     },
     servers: [
       {
